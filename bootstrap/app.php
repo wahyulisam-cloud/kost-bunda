92ddb30e8dd7ebbd1277062be92b272211b55800
wahyu lisa <wahyulisam@gmail.com>
2026-04-16 07:38:29 +0700

    fix proxy & session

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ]);
 
     $middleware->trustProxies(at: '*');
+    $middleware->append(\App\Http\Middleware\TrustProxies::class);
+
 })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
